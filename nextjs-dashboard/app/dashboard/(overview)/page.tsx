a3bbbaa1f450d97f43b23afe8c9d00859b88806d
wcamer <xeus.xiii@hotmail.com>
2024-06-20 19:07:22 -0600

    Ch 9 Streaming updates completed

---
+++ nextjs-dashboard/app/dashboard/(overview)/page.tsx
@@ -0,0 +1,43 @@
+import { Card } from '@/app/ui/dashboard/cards';
+import RevenueChart from '@/app/ui/dashboard/revenue-chart';
+import LatestInvoices from '@/app/ui/dashboard/latest-invoices';
+import { lusitana } from '@/app/ui/fonts';
+// import { fetchRevenue, fetchLatestInvoices, fetchCardData } from '@/app/lib/data';
+//removed "fetchRevenue" and "fetchLatestInvoices" as per instructions in chapter 9: Streaming
+import {fetchCardData } from '@/app/lib/data';
+import { Suspense } from 'react';
+import { LatestInvoicesSkeleton, RevenueChartSkeleton, CardsSkeleton } from '@/app/ui/skeletons';
+import CardWrapper from '@/app/ui/dashboard/cards';
+
+
+export default async function Page() {
+  // const revenue = await fetchRevenue() // removed this line as per instructions in chapater 9: streaming
+  // const latestInvoices = await fetchLatestInvoices() // removed this line as per instructions in ch 9: streaming
+  const cardData = await fetchCardData()
+  const totalPaidInvoices = cardData.totalPaidInvoices
+  const totalPendingInvoices = cardData.totalPendingInvoices
+  const numberOfInvoices = cardData.numberOfInvoices
+  const numberOfCustomers = cardData.numberOfCustomers
+  return (
+    <main>
+      <h1 className={`${lusitana.className} mb-4 text-xl md:text-2xl`}>
+        Dashboard
+      </h1>
+      <div className="grid gap-6 sm:grid-cols-2 lg:grid-cols-4">
+       <Suspense fallback={<CardsSkeleton />}>
+          <CardWrapper />
+       </Suspense>
+      </div>
+      <div className="mt-6 grid grid-cols-1 gap-6 md:grid-cols-4 lg:grid-cols-8">
+        <Suspense fallback={<RevenueChartSkeleton />}>
+           <RevenueChart />
+        </Suspense>
+       
+       <Suspense fallback={<LatestInvoicesSkeleton />}>
+        <LatestInvoices  />
+       </Suspense>
+        
+      </div>
+    </main>
+  );
+}
