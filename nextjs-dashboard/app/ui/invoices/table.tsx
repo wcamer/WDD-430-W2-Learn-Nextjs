9dbabb810400876c1997bff134e0448974fa6d34
wcamer <xeus.xiii@hotmail.com>
2024-06-22 14:38:22 -0600

    ch 14 improving accessibility updates

--- nextjs-dashboard/app/ui/invoices/table.tsx
+++ nextjs-dashboard/app/ui/invoices/table.tsx
@@ -31,7 +31,8 @@ export default async function InvoicesTable({
                         className="mr-2 rounded-full"
                         width={28}
                         height={28}
-                        alt={`${invoice.name}'s profile picture`}
+                        alt={`${invoice.name}'s profile picture`
+                      }
                       />
                       <p>{invoice.name}</p>
                     </div>
