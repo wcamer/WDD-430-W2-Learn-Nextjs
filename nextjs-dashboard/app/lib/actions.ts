273958ada8413bf94a37a36c88fe558a882bd2e6
wcamer <xeus.xiii@hotmail.com>
2024-06-22 13:47:06 -0600

    ch 13 Handling erros updates

--- nextjs-dashboard/app/lib/actions.ts
+++ nextjs-dashboard/app/lib/actions.ts
@@ -31,10 +31,16 @@ const {customerId, amount, status} = CreateInvoice.parse({
   })
   const amountInCents = amount * 100
   const date = new Date().toISOString().split('T')[0]
-
-  await sql `INSERT INTO invoices (customer_id, amount, status, date)
-            VALUES (${customerId}, ${amountInCents}, ${status}, ${date})
-            `
+ try{
+    await sql `INSERT INTO invoices (customer_id, amount, status, date)
+                VALUES (${customerId}, ${amountInCents}, ${status}, ${date})
+                `
+ } catch (error) {
+    return {
+        message: 'Database Error: Failed to Create Invoice'
+    }
+ }
+ 
 
   revalidatePath('/dashboard/invoices')
   redirect('/dashboard/invoices')
@@ -51,18 +57,36 @@ export async function updateInvoice(id: string, formData: FormData) {
     });
    
     const amountInCents = amount * 100;
-   
-    await sql`
+    try{
+        await sql`
       UPDATE invoices
       SET customer_id = ${customerId}, amount = ${amountInCents}, status = ${status}
       WHERE id = ${id}
     `;
+    }catch (error) {
+        return {
+            message: "Database Error: Failed to update Invoice"
+        }
+    }
+    
    
     revalidatePath('/dashboard/invoices');
     redirect('/dashboard/invoices');
   }
 
   export async function deleteInvoice(id: string) {
-    await sql`DELETE FROM invoices WHERE id = ${id}`;
-    revalidatePath('/dashboard/invoices');
+
+    throw new Error('Failed to Delete Invoice'); // this line will be deleted soon
+
+    try{
+        await sql`DELETE FROM invoices WHERE id = ${id}`;
+        revalidatePath('/dashboard/invoices')
+        return {message: 'Invoice Deleted Successfully'}
+    }catch (error){
+        return {
+            message: 'Database error: Failed to delete invoice.  Operation canceled'
+        }
+    }
+    
+    ;
   }
