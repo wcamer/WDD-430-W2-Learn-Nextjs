250474b61c5a62055b52360878cbbd1d17f54140
wcamer <xeus.xiii@hotmail.com>
2024-06-22 15:36:30 -0600

    ch 15 Adding Authentication updates

--- nextjs-dashboard/app/lib/actions.ts
+++ nextjs-dashboard/app/lib/actions.ts
@@ -4,6 +4,8 @@ import {z} from 'zod'
 import {sql} from '@vercel/postgres'
 import { revalidatePath } from 'next/cache'
 import {redirect} from 'next/navigation'
+import {signIn} from '@/auth'
+import { AuthError } from 'next-auth'
 
 const FormSchema = z.object({
     id: z.string(),
@@ -149,4 +151,23 @@ export async function updateInvoice(
     }
     
     ;
+  };
+
+  export async function authenticate(
+    prevState: string | undefined,
+    formData: FormData,
+  ){
+    try {
+        await signIn('credentials', formData);
+    }catch (error) {
+        if(error instanceof AuthError){
+            switch (error.type){
+                case 'CredentialsSignin':
+                    return 'Invalid credentials.'
+                default:
+                    return 'Something went wrong.'
+            }
+        }
+        throw error;
+    }
   }
